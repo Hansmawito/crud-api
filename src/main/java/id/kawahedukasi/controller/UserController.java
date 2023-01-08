0953cf1d5651414ee754ddda01aab7add59af45a
Hans Panggabean <hansmawito.p@gmail.com>
2023-01-08 20:15:44 +0700

    day 2 endpoint input

--- src/main/java/id/kawahedukasi/controller/UserController.java
+++ src/main/java/id/kawahedukasi/controller/UserController.java
@@ -1,18 +1,45 @@
 package id.kawahedukasi.controller;
 
+import javax.json.JsonObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Map;
 
 @Path("/users")
 @Produces(MediaType.TEXT_PLAIN)
 public class UserController {
     @POST
-    public String create(){
-        return "SUCCES CREATE";
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Map<String, Object> create(JsonObject request){
+//        String name = (String) request.get("name");
+//        String gender = (String) request.get("gende");
+        return Map.of(
+                "data", request
+
+        );
+    }
+
+    @GET
+    public Map<String, Object> getByNameGebderPob(@QueryParam("name") String name,
+                                                  @QueryParam("gender") String gender,
+                                                  @QueryParam("pob") String pob){
+        return Map.of(
+                "id", -99,
+                "nama", name,
+                "gender", gender,
+                "pob",pob
+        );
+
     }
     @GET
-    public String get(){
-        return "SUKSES GET";
+    @Path("/name/{name}")
+    public Map<String, Object> get(@PathParam("name") String name){
+        return Map.of(
+                    "id", -99,
+                    "nama", name,
+                    "gender", ""
+            );
+
     }
     @PUT
     public String update(){
